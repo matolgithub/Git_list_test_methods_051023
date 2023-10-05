88891ba4e09c2afe40217c64037b32f9e3196b65
Oleg <89175955650@mail.ru>
2023-10-05 22:42:57 +0300

    Next commit to GitHub.

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-def test_list():
-    print("It's really all right!")
+class SimpleList:
+    def __init__(self, simple_list=[1, 6, 9, 4, 2, 1, 9, 15]):
+        self.simple_list = simple_list
+
+    def test_list(self):
+        print("It's really all right!")
 
 
 if __name__ == "__main__":
-    test_list()
+    test_list_object = SimpleList()
+    test_list_object.test_list()
